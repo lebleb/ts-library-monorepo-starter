ee9c6ef18010cc51c493ee1425caaf90eda00439
Sonat Alpagut <sonat@alpagut.net>
2023-04-22 15:52:29 +0200

    feat: Add Yarn workspace, TypeScript, and linting configuration

---
+++ .eslintrc.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+    extends: 'standard-with-typescript',
+    parserOptions: {
+        project: './tsconfig.base.json'
+    },
+    root: true,
+};
---
+++ packages/library-2/src/index.ts
@@ -0,0 +1,3 @@
+import { sum } from 'library-1/src'
+
+export const printSum = (a: number, b: number): void => { console.log(sum(a, b)) }
