40861c037942461cb26f897a506c86a83db675e8
Sonat Alpagut <sonat@alpagut.net>
2023-04-28 16:37:53 +0200

    feat: add diff fn

--- packages/library-1/src/index.spec.ts
+++ packages/library-1/src/index.spec.ts
@@ -1,8 +1,15 @@
 import { describe, expect, test } from '@jest/globals'
 import { sum } from './sum'
+import { diff } from './diff'
 
 describe('sum module', () => {
   test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3)
+  })
+})
+
+describe('diff module', () => {
+  test('diifs 2 - 1 to equal 1', () => {
+    expect(diff(2, 1)).toBe(1)
   })
 })
