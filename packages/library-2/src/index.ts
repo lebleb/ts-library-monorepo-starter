08fc3c9ef283bc5549e3e04350fd0ae0b35f7657
Sonat Alpagut <sonat@alpagut.net>
2023-04-28 16:44:38 +0200

    feat: update package names

--- packages/library-2/src/index.ts
+++ packages/library-2/src/index.ts
@@ -1,6 +1,6 @@
 /* istanbul ignore file */
 
-import { sum } from 'library-1'
+import { sum } from 'lebleb-library-1'
 
 export const printSum = (a: number, b: number): void => { console.log(sum(a, b)) }
 
