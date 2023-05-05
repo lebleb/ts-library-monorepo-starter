ee073b2f834940815247a6d3dd01cb833103ff82
Sonat Alpagut <sonat@alpagut.net>
2023-05-05 15:51:25 +0200

    feat: use specail calculation

--- packages/library-2/src/index.ts
+++ packages/library-2/src/index.ts
@@ -1,7 +1,7 @@
 /* istanbul ignore file */
 
-import { division, sum } from 'lebleb-library-1'
+import { division, special, sum } from 'lebleb-library-1'
 
-export const printCalculation = (a: number, b: number): void => { console.log(sum(a, b), division(4, 2)) }
+export const printCalculation = (a: number, b: number): void => { console.log(sum(a, b), division(4, 2), special(1, b, 3)) }
 
 printCalculation(1, 2)
